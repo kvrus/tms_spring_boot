b5fc694ed9c913ea6b23d684a645f0f0e1521ff0
kvrus <kv.russia@gmail.com>
2024-08-13 16:00:13 +0300

    signup user method added. But we did not get JWT tokens to login to the system

--- tms/src/main/java/ru/moscow/tms/TmsApplication.java
+++ tms/src/main/java/ru/moscow/tms/TmsApplication.java
@@ -10,9 +10,12 @@ import ru.moscow.tms.models.UserEntity;
 import ru.moscow.tms.repository.RoleRepository;
 import ru.moscow.tms.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static ru.moscow.tms.security.AuthUtil.SUPERUSER_ROLE;
+import static ru.moscow.tms.security.AuthUtil.USER_ROLE;
 
 @SpringBootApplication
 public class TmsApplication {
@@ -28,6 +31,7 @@ public class TmsApplication {
 			Role role = roleRepository.findByName(SUPERUSER_ROLE).orElseThrow(() -> new IllegalStateException("Super role not found"));
 			superuser.setUsername("admin");
 			superuser.setPassword(encoder.encode("admin")); // generate random password and save it to file, let devops fetch it and change to some secure one
+			superuser.setRoles(Collections.singletonList(role));
 			superuser.setRoles(List.of(role));
 			if (!repository.existsByUsername(superuser.getUsername())) {
 				repository.save(superuser);
--- tms/src/main/java/ru/moscow/tms/models/UserEntity.java
+++ tms/src/main/java/ru/moscow/tms/models/UserEntity.java
@@ -28,7 +28,7 @@ public class UserEntity implements UserDetails {
 
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private List<Role> roles = new ArrayList<>();
