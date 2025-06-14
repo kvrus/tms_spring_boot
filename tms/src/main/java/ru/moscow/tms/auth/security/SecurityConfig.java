9834b02e175f0cab2345cc454637003218621baa
koss <kv.russia@gmail.com>
2025-06-14 23:39:48 +0300

    GUI tests added Need to rewrite it all to maven multi-module project

--- tms/src/main/java/ru/moscow/tms/auth/security/SecurityConfig.java
+++ tms/src/main/java/ru/moscow/tms/auth/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                                          "/images/**",
                                          "/v3/api-docs/**",
                                          "/swagger-ui/**",
+                                         "/registration",
                                          "/swagger-resources/**",
                                          "/v2/api-docs/**").permitAll()
                         .requestMatchers("/admin/**").hasAuthority(SUPERUSER_ROLE)
