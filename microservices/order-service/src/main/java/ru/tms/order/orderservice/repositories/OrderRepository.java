34722177b447e3f89814831744ee34ce52ed9760
koss <kv.russia@gmail.com>
2025-07-06 14:36:36 +0300

    maven prepared

--- microservices/order-service/src/main/java/ru/tms/order/orderservice/repositories/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.tms.order.orderservice.repositories;
-import ru.tms.order.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservices/product-service/src/main/java/ru/tms/product/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.tms.product.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.tms.product.productservice.models.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
